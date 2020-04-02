197cfaa26b3819a756b0369d76550e9d4ba73ed2
isaac <isaac.ingles@gmail.com>
2020-04-01 19:37:47 -0600

    got basic layout done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
-    <div class="container-fluid container-tw">
-      <div class="container">
+    <b-navbar fixed="top" class="container-fluid container-tw">
+      <div class="container top">
         <h1>Twitter Clone</h1>
+        <b-form>
+          <b-input-group>
+            <b-input-group-prepend is-text>
+              <b-icon icon="search"></b-icon>
+            </b-input-group-prepend>
+            <b-form-input type="search" placeholder="search"></b-form-input>
+          </b-input-group>
+        </b-form>
+        <b-button-group class="signin-controls">
+          <b-button>Log in</b-button>
+          <b-button>Sign Up</b-button>
+        </b-button-group>
+        <b-button>
+          <b-icon icon="three-dots"></b-icon>
+        </b-button>
       </div>
-    </div>
+    </b-navbar>
 
     <div class="container-fluid container-tw">
       <div class="container">
@@ -33,12 +48,25 @@
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
-<style lang="less" scoped>
+<style lang="less">
 @twblue: #15202B;
 @twgray: #CCD6DD;
+
+html {
+  background: @twblue;
+}
+
+body {
+  font-size: 1.6rem;
+}
+
+.top {
+  display: flex;
+}
 
 h1 {
   font-size: 2rem;
@@ -59,17 +87,16 @@ p {
 }
 
 .main-content {
-  border-left: 1px
-  solid @twgray;
+  border-left: 1px solid @twgray;
   border-right: 1px solid @twgray;
-  padding: 20px;
+  padding: 6rem 2rem 2rem;
 }
 
 .side-content {
   border: 1px solid @twgray;
   border-radius: 10px;
-  margin: 20px 10px;
-  padding:20px;
+  margin: 6rem .5rem 1rem;
+  padding: 2rem;
 }
 
 </style>
